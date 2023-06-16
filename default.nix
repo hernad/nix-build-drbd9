5d23934128e49dfd3edc69d4eeb0fddda0a6c141
Ernad Husremovic <hernad@bring.out.ba>
2023-06-16 12:37:40 +0200

    build drdb

--- default.nix
+++ default.nix
@@ -26,11 +26,7 @@ stdenv.mkDerivation rec {
       git
   ];
 
-  src = pkgs.fetchgit {
-    url = "https://gitlab.inria.fr/nix-tutorial/chord-tuto-nix-2022";
-    rev = "069d2a5bfa4c4024063c25551d5201aeaf921cb3";
-    sha256 = "sha256-MlqJOoMSRuYeG+jl8DFgcNnpEyeRgDCK2JlN9pOqBWA=";
-  };
+  src = ./drbd;
 
 
   #configurePhase = ''
